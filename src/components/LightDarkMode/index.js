ec727adbfc9c07fa8816b882e41db21428d6108f
= <=>
2024-01-09 20:56:32 +0000

    commit

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,40 @@
+// Write your code here
+import './index.css'
+import {Component} from 'react'
+class LighDarkMode extends Component {
+  state = {
+    mode: 'Light Mode',
+    headClass: 'headingLight',
+    bgClass: 'dark',
+    buttonClass: 'buttonLight',
+  }
+  onclicking() {
+    if (mode === 'Light Mode') {
+      return this.setState(prev => ({
+        mode: (prev.mode = 'Dark Mode'),
+        headClass: (prev.headClass = 'headingDark'),
+        bgClass: (prev.bgClass = 'light'),
+        buttonClass: (prev.buttonClass = 'buttonDark'),
+      }))
+    } else {
+      return this.setState(prev => ({
+        mode: (prev.mode = 'Light Mode'),
+        headClass: (prev.headClass = 'headingLight'),
+        bgClass: (prev.bgClass = 'dark'),
+        buttonClass: (prev.buttonClass = 'buttonLight'),
+      }))
+    }
+  }
+  render() {
+    const {mode, headClass, bgClass, buttonClass} = this.state
+    return (
+      <div className={bgClass}>
+        <h1 className={headClass}>Click To change mode</h1>
+        <button className={buttonClass} onClick={this.onclicking}>
+          {mode}
+        </button>
+      </div>
+    )
+  }
+}
+export default LighDarkMode
